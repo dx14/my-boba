3eb52c740084331496a96379f04973b00ed7f229
Dennis Xu <dx14@duke.edu>
2020-07-01 17:24:13 -0400

    remove console logs, update heroku config

--- src/services/base.js
+++ src/services/base.js
@@ -2,7 +2,6 @@ const axios = require('axios')
 
 const getBaseUrl = () => {
     let baseUrl
-    console.log(process.env.NODE_ENV)
     if (process.env.NODE_ENV === 'development') {
       baseUrl = "http://localhost:8080/";
     } else {
